ee47bad4f626f455e9ff477caf3a5757bb97cc75
BelikJulia <43135868+BelikJulia@users.noreply.github.com>
2022-03-15 15:06:12 +0300

    Add files via upload

---
+++ pp_2020_spring/build/3rdparty/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: E:/pp_2020_spring/3rdparty/gtest/googlemock
+# Build directory: E:/pp_2020_spring/build/3rdparty/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ pp_2020_spring/build/3rdparty/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/pp_2020_spring/3rdparty/gtest/googletest
+# Build directory: E:/pp_2020_spring/build/3rdparty/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
